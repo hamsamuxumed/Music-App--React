12ec23f2074a217b50d75bb9997ac11b86e87e6a
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2021-11-01 14:53:09 +0000

    initial files

---
+++ config/dev.webpack.config.js
@@ -0,0 +1,10 @@
+const config = require('./webpack.config.js');
+
+config.devServer = {
+  historyApiFallback: true,
+  port: 8080,
+  liveReload: true
+};
+config.devtool = 'inline-source-map';
+
+module.exports = config;
