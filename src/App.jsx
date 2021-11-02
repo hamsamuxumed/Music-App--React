f83a6281d83c62e0a614b069a59657ebc93c26cf
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2021-11-02 17:42:01 +0000

    connect all  components to display on localhost

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Summary } from "./components/SummaryCard"
+import { Album } from "./components/Album";
 
 const App = () => {
     return (
         <>
             <h1>Music App</h1>
             <Summary />
+            <Album />
+
         </>
     )
 }
---
+++ src/components/Album.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Albums} from './Albums'
+
+
+
+
+export const Album = () => {
+    
+    return (
+        <>
+            <Albums />
+        </>
+    )
+}
