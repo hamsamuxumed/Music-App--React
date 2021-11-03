fd2fb4cf234bd2f7b62ffe1974064210a4f8c5ec
ZNBrown <ZachNatBrown@gmail.com>
2021-11-03 18:25:39 +0000

    adds tracklist plus error message

--- src/components/Albums/index.jsx
+++ src/components/Albums/index.jsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { LikeButton } from '../LikeButton';
 import './style.css';
+const token = "Bearer BQDIxTfIBT0AU8hRmzYaMZsV_7yi-wKb6c-uX2dtzIxlISRrP6IqPc-20xUxG0qbDe7wD9wRTrnN10OqOOhCPg49DAKXuZMkJlvhozUZvuppq_WAhLgp8IaBQCZg5NUB5-DlQ4nMOSBXh9V9JO2YxmXCmSa4jhfY5O0"
 
 export const Albums = () => {
-
   const [ albums, setAlbums ] = useState([])
 
   useEffect(() => {
@@ -13,15 +13,29 @@ export const Albums = () => {
               let { data } = await axios.get('https://api.spotify.com/v1/artists/7M1FPw29m5FbicYzS2xdpi/albums?include_groups=album&market=GB&limit=20&offset=10', { headers: {
                 "Accept": "application/json",
                 "Content-Type": "application/json", 
-                "Authorization" : "Bearer BQBZzj2gnfxAb6d2nJlbxKXWr2hAOyPqoXZ5v4xmIoSR6ZdBptdfinwuz5BhkFapihkiBw0Zunhdhbr6FsHQEZfmKTquc51lJ7X8mxhFUbXKCyDc5WcIncO9kvQEiSJ4CiuR8Q-6gGwuHUEHUQ4fCyxiw_daeY80HdY"  
+                "Authorization" : `${token}`  
               }});
+              for (const album of data.items) {
+                let albumId = album.uri.split(":")[2]
+                let tracklist = await axios.get(`https://api.spotify.com/v1/albums/${albumId}/tracks?offset=0&limit=20&market=GB`, { headers: {
+                  "Accept": "application/json",
+                  "Content-Type": "application/json", 
+                  "Authorization" : `${token}`  
+                }});
+                album.tracklist = tracklist.data.items
+              }
               setAlbums(data.items)
-              console.log(data.items)
           } catch(err) {
-              console.warn(err);
+            console.log(`Error ${err}. If this is a get failed i attempted to resolve this the nice way using a spotify app but its giving me issues. Will hopefully fix soon` )
           }
       }
       fetchAlbums()
+
+      
+
+
+
+//
   }, [])
 
   
@@ -36,9 +50,9 @@ export const Albums = () => {
                     <p>Released: {st.release_date}</p>
                     <p>Total tracks: {st.total_tracks}</p>
                         <p>On spotify: {st.external_urls.spotify}</p>
-                        {/* <ol>
-                          {st.tracklist.map(tr => (<li>{tr}</li>))}
-                        </ol> */}
+                        <ol>
+                          {st.tracklist.map(tr => (<li>{tr.name}</li>))}
+                        </ol>
                     </ul>
                   </div>
                 ))
